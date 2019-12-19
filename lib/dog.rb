a9b95107d458053f0668f96c32a54688bdb2018f
Joanna E K <kalemajoanna@gmail.com>
2019-12-19 15:59:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,14 @@
 # Add your code here
 class Dog 
-  
+  @@all = []
   def initialize
-    
+    @@all << self 
+  end 
+  
+  def self.all 
+    @all 
+  end 
+  
+  def self.print_all
+    counter 1 
 end 
